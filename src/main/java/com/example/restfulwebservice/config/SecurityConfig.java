b27a0fc2c990b5bcb85f9750d8b0f2558233ba15
12345 <ing2895@naver.com>
2020-07-10 16:15:27 +0900

    section4

---
+++ src/main/java/com/example/restfulwebservice/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.example.restfulwebservice.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+		auth.inMemoryAuthentication()
+			.withUser("ing2895")
+			.password("{noop}k1mjung1")
+			.roles("USER");
+		
+			
+	}
+
+}
